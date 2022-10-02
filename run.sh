d448a136d672374468b637b261c3b8207c0be49e
Nikolai Perlt <nikolaiperlt@outlook.com>
2022-10-02 19:41:01 +0000

    stuff

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "msys"* ]]; then
-  docker run -v /var/run/docker.sock:/var/run/docker.sock -it arch-workspace bash
+	docker run -v //var/run/docker.sock:/var/run/docker.sock -it arch-workspace bash
 else
-  docker run -v //var/run/docker.sock:/var/run/docker.sock -it arch-workspace bash
+	docker run -v /var/run/docker.sock:/var/run/docker.sock -it arch-workspace bash
 fi
