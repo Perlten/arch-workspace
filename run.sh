1d6fc1d0b6f555cb38de28e8448d05d6bd5bf19b
Nikolai Perlt <nikolaiperlt@outlook.com>
2022-10-02 19:42:34 +0000

    print os on run

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "msys"* ]]; then
+	echo "Running on windows"
 	docker run -v //var/run/docker.sock:/var/run/docker.sock -it arch-workspace bash
 else
+	echo "Running on linux"
 	docker run -v /var/run/docker.sock:/var/run/docker.sock -it arch-workspace bash
 fi
